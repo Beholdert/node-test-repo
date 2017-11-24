043c8b4cf2d650b7931e8cada662999160e10da0
SanOk <you@example.com>
2017-11-24 20:20:07 +0600

    asdasd

--- app.js
+++ app.js
@@ -14,28 +14,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.render('index');
+    // res.render('index');
+    res.send('<h2>COCИ COCK!!!!!!</h2>');
 });
 
-app.post('/signUp', (req, res) => {
-    users.db.push(req.body);
-    res.render('index');
-    console.log(req.body);
-});
+// app.post('/signUp', (req, res) => {
+//     users.db.push(req.body);
+//     res.render('index');
+//     console.log(req.body);
+// });
 
-app.post('/logIn', (req, res) => {
-    console.log(users.db);
-    if (users.find(req.body.login, req.body.password)) {
-        res.render('app', {
-            books: books
-        });
-    } else {
-        res.render('err')
-    }
-});
+// app.post('/logIn', (req, res) => {
+//     console.log(users.db);
+//     if (users.find(req.body.login, req.body.password)) {
+//         res.render('app', {
+//             books: books
+//         });
+//     } else {
+//         res.render('err')
+//     }
+// });
 
-app.get('/books', function(req, res) {
-    res.json(books);
-});
+// app.get('/books', function(req, res) {
+//     res.json(books);
+// });
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
